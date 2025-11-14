f66505147eef27685ea41231fb6cc3c750d75f6b
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-11-15 01:13:09 +0600

    statice in typescript

---
+++ src/video7.ts
@@ -0,0 +1,33 @@
+// ? Static in typescript -> static
+
+class Counter {
+  static count: number = 0;
+
+  static increment() {
+    return (Counter.count += 1);
+  }
+
+  static decrement() {
+    return (Counter.count -= 1);
+  }
+}
+
+// const Counter = new Counter();
+
+// console.log(instance1.increment());
+// console.log(instance1.increment());
+// console.log(instance1.increment());
+
+// const instance2 = new Counter();
+
+// console.log(instance2.increment());
+// console.log(instance2.increment());
+// console.log(instance2.increment());
+
+console.log(Counter.increment());
+console.log(Counter.increment());
+console.log(Counter.increment());
+
+console.log(Counter.increment());
+console.log(Counter.increment());
+console.log(Counter.increment());
