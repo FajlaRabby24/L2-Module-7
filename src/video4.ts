7ea7d1dcfefc7bc2e568336b7ebd711a53bd7f6c
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-11-14 23:38:09 +0600

    type guard using instance of

---
+++ src/video4.ts
@@ -0,0 +1,51 @@
+//oop : instance of type guard/ type narrowing
+
+class Person {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  getSleep(numOfhours: number) {
+    console.log(`${this.name} doinik ${numOfhours} ghonta ghumai`);
+  }
+}
+
+class Student extends Person {
+  doStudy(numOfhours: number) {
+    console.log(`${this.name} doinik ${numOfhours} ghonta study koe`);
+  }
+}
+
+class Teacher extends Person {
+  takeClass(numOfhours: number) {
+    console.log(`${this.name} doinik ${numOfhours} ghonta class nei`);
+  }
+}
+
+// function guard
+
+const isStudent = (user: Person) => {
+  return user instanceof Student; // * true or false
+};
+
+const isTeacher = (user: Person) => {
+  return user instanceof Teacher; // * true or false
+};
+
+const getUserInfo = (user: Person) => {
+  if (isStudent(user)) {
+    user.doStudy(10);
+  } else if (isTeacher(user)) {
+    user.takeClass(5);
+  } else {
+    user.getSleep(15);
+  }
+};
+
+const student1 = new Student("Mr. student");
+const teacher1 = new Teacher("Mr. teacher");
+const person1 = new Person("Mr. Person");
+
+getUserInfo(person1);
